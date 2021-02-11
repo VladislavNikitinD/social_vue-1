8ad71ee676c9b2c2beb6397b78aa8f3633e14c3e
Admin <nikitin.vladislav@mail.ru>
2021-02-11 20:16:57 +0300

    vuex lesson

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,14 @@
       <v-list nav shaped dense> 
         <v-list-item two-line>
           <v-list-item-avatar>
-            <img src="https://randomuser.me/api/portraits/men/2.jpg">
+            <img v-bind:src=" `https://randomuser.me/api/portraits/men/${ $store.state.userData.id }.jpg` ">
           </v-list-item-avatar>
           <v-list-item-content>
             <v-list-item-title>
-              SocialLink
+              {{title}}
             </v-list-item-title>
             <v-list-item-subtitle>
-              Руслан Абкадиров
+              {{$store.state.userData.name}}
             </v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
@@ -60,11 +60,10 @@
 <script>
 export default {
   name: "App",
-
   components: {},
-
   data: () => ({
     nav: true,
+    title: 'SocialVue'
   }),
 };
 </script>
